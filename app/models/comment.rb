8c4033e8bc087d33fdb7faa62ae57ea02ed7521e
philschoof <phil.schoof@gmail.com>
2016-06-17 15:56:37 -0400

    Add associations to models
    Add belongs_to to ratings and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ActiveRecord::Base
+  belongs_to :user, inverse_of: :comments
+  belongs_to :keg, inverse_of: :comments
+  validates_presence_of :content
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,5 @@
 class Rating < ActiveRecord::Base
+  belongs_to :user, inverse_of: :ratings
+  belongs_to :keg, inverse_of: :ratings
+  validates_presence_of :score
 end
